736dc78dbced77b5654d8f6fd76d28bd74971f2b
rijasm09@gmail.com <rijasm09@gmil.com>
2023-03-23 11:11:58 +0530

    updated api

--- api/app.js
+++ api/app.js
@@ -1,18 +1,18 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const { fetchUrlContent } = require("./fetch-url-content")
-const { clean } = require("./clean")
-const { count } = require("./count")
+// const express = require("express")
+// const app = express()
+// const cors = require("cors")
+// const { fetchUrlContent } = require("./fetch-url-content")
+// const { clean } = require("./clean")
+// const { count } = require("./count")
 
-app.use(cors())
-app.use(express.urlencoded(({ extended: true })))
+// app.use(cors())
+// app.use(express.urlencoded(({ extended: true })))
 
-app.use((req,res,next)=> {
-    console.log('here');
-    next()
-})
+// app.use((req,res,next)=> {
+//     console.log('here');
+//     next()
+// })
 
 
 
-module.exports = app
+// module.exports = app
--- api/count.js
+++ api/count.js
@@ -1,12 +1,6 @@
 exports.count = string => {
-    let map = {}
     const words = string.split(" ").filter(word => word !== "")
     const totalWords = words.length
-  
-    for (let i = 0; i < words.length; i++) {
-      const item = words[i]
-      map[item] = (map[item] + 1) || 1
-    }
-  
+
     return {totalWords }
   }
